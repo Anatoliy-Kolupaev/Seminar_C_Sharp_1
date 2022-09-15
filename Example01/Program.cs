3f4f5122f6b64620de6269a7ef9a5f735fb4bcfa
Anatoliy <778966@mail.ru>
2022-09-15 23:46:42 +0500

    added game

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,12 +1,11 @@
 ﻿Console.WriteLine("Мы попали в игру угадай число");
-int secret_number = new Random().Next(1,11);
+int secret_number = new Random().Next(1, 11);
 int attempt = 5;
 bool Win = false;
 while (attempt > 0)
 {
     Console.WriteLine("Введите число от 1 до 10");
     int user_number = int.Parse(Console.ReadLine());
-    Console.WriteLine(user_number);
     if (user_number > secret_number)
     {
         Console.WriteLine("Секретное число меньше");
@@ -17,18 +16,18 @@ while (attempt > 0)
         Console.WriteLine("Секретное число больше");
         attempt--;
     }
-    else if(user_number == secret_number)
+    else if (user_number == secret_number)
     {
         Console.WriteLine("Мы угадали");
         Win = true;
         break;
     }
-    Console.WriteLine("По-пробуй еще раз, осталось " + attempt +" попытки");
+    Console.WriteLine("По-пробуй еще раз, осталось " + attempt + " попытки");
 }
-if(Win)
+if (Win)
 {
     Console.WriteLine("Вы победили приходи еще");
-    Console.WriteLine("Осталось попыток " + (attempt-1));
+    Console.WriteLine("Осталось попыток " + (attempt - 1));
 }
 else
 {
